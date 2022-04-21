4cb3e2a40af8f6b948bf16d4d787be2866298bd4
Samuel Maftoul <samuel.maftoul@gmail.com>
2022-04-21 19:18:27 +0200

    Update bootstrap script to support home-manager

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+set -e
+
 groupadd nixbld -g 30000 || true
 
 for i in {1..10}; do
@@ -10,7 +12,20 @@ curl -L https://nixos.org/nix/install | sh
 
 . /root/.nix-profile/etc/profile.d/nix.sh
 
+NIX_CHANNEL="nixos-unstable"
+nix-channel --remove nixpkgs
+nix-channel --add "https://nixos.org/channels/$NIX_CHANNEL" nixos
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+export NIXOS_CONFIG="/tmp/nix-master/configuration.nix"
+
+nix-env --set \
+  -I nixpkgs=/root/.nix-defexpr/channels/nixos \
+  -I home-manager=/root/.nix-defexpr/channels/home-manager \
+  -f '<nixpkgs/nixos>' \
+  -p /nix/var/nix/profiles/system \
+  -A system
 
 curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | \
 	NIX_CHANNEL=nixos-unstable \
